d70b230b363581a073d8b9751080e4bbacc3c745
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-08-07 08:20:23 +0300

    Added missing whitespaces and lines

--- src/snake.py
+++ src/snake.py
@@ -1,8 +1,9 @@
 import pygame
 
+
 class Snake:
     """Snake class. The snake updates and draws itself."""
-    def __init__(self,coords):
+    def __init__(self, coords):
         """Snake initialiation.
         Class variables:
         self.direction -- The direction where the snake is going atm, 'L'/'R'/'U'/'D'
@@ -16,7 +17,7 @@ class Snake:
         # In the beginning, the body is just one element long.
         self.body = coords
 
-    def update(self,n,m,apple):
+    def update(self, n, m, apple):
         """Snake update method.
         Status, direction and possible death are processed here.
         Arguments:
@@ -24,19 +25,19 @@ class Snake:
         apple -- Where is the apple atm, tuple (int,int)
         """
         # 0:normal, 1: ate apple, 2: died
-        status=1
+        status = 1
         # Update x and y based on direction
-        if self.direction=='U': x=0;y=-1;
-        if self.direction=='D': x=0;y=1;
-        if self.direction=='R': x=1;y=0;
-        if self.direction=='L': x=-1;y=0;
+        if self.direction == 'U': x = 0; y = -1;
+        if self.direction == 'D': x = 0; y = 1;
+        if self.direction == 'R': x = 1; y = 0;
+        if self.direction == 'L': x = -1; y = 0;
         # b is the last element of the list body, means the snake's head
         b = self.body[-1]
         # This is where the snake's head will be next
-        b = (b[0]+x,b[1]+y)
+        b = (b[0] + x, b[1] + y)
         # If snake hits itself or outside playfield, it dies
-        if b in self.body or b[0]>n-1 or b[0]<0 or b[1]>m-1 or b[1]<0:
-            status=2
+        if b in self.body or b[0] > n - 1 or b[0] < 0 or b[1] > m - 1 or b[1] < 0:
+            status = 2
             return status
         # Move one space
         self.body.append(b)
@@ -44,15 +45,15 @@ class Snake:
         # otherwise keep it, which makes the snake longer
         if b != apple:
             self.body.pop(0)
-            status=0
+            status = 0
         return status
 
-    def draw(self,screen,scale):
+    def draw(self, screen, scale):
         """The draw method for Snake.
         Arguments:
         screen -- Pygame screen where to draw
         scale -- The scale parameter used in the game
         """
         for i in self.body:
-            rectangle = (i[0]*scale,i[1]*scale,scale,scale)
-            pygame.draw.rect(screen,(0,50,0),rectangle)
+            rectangle = (i[0]*scale, i[1] * scale, scale, scale)
+            pygame.draw.rect(screen, (0, 50, 0), rectangle)
