9d622e17e4dc7d838e256b15f05b7326036af0e3
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-08-17 08:45:40 +0300

    Added apple location update test

--- tests/test_apple.py
+++ tests/test_apple.py
@@ -1,7 +1,19 @@
 import unittest
 from apple import Apple
+from snake import Snake
 
 
 class AppleTest(unittest.TestCase):
     def test_update(self):
-        pass
+        """Test that apple location is correctly updated"""
+        # Logical size of the playfield
+        n = 10
+        m = 10
+        # Create snake and apple
+        snake = Snake([(1, 3), (2, 3), (3, 3)])
+        apple = Apple((5, 5))
+        # Update apple
+        apple.update(snake.body, n, m)
+        # Make sure apple is not outside the playfield
+        self.assertTrue(0 <= apple.location[0] <= n)
+        self.assertTrue(0 <= apple.location[1] <= m)
