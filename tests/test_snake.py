d6423b73a311a326e8242f0d5703a419d9c7cd25
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-08-25 08:39:24 +0300

    Added test for checking snake death

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -24,3 +24,27 @@ class SnakeTest(unittest.TestCase):
         x, y = snake.set_x_y()
         self.assertEqual(x, -1)
         self.assertEqual(y, 0)
+
+    def test_check_death(self):
+        """Test that snake's death correctly processed."""
+        snake = Snake([(2, 2), (2, 1)])
+        n, m = 2, 2
+        # Update the coordinate changes based on where the snake is headed
+        x, y = snake.set_x_y()
+        # b is the last element of the list body, means the snake's head
+        b = snake.body[-1]
+        # This is where the snake's head will be next
+        b = (b[0] + x, b[1] + y)
+        # Check if the snake dies
+        self.assertTrue(snake.check_death(b, n, m))
+        # Now the same when the snake should stay alive
+        snake = Snake([(2, 2), (2, 1)])
+        n, m = 4, 4
+        # Update the coordinate changes based on where the snake is headed
+        x, y = snake.set_x_y()
+        # b is the last element of the list body, means the snake's head
+        b = snake.body[-1]
+        # This is where the snake's head will be next
+        b = (b[0] + x, b[1] + y)
+        # Check if the snake dies
+        self.assertFalse(snake.check_death(b, n, m))
