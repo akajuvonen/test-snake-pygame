a4c4bc7f15b7125441acd56d893a2fef46d5b139
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-08-25 08:40:06 +0300

    Corrected test_set_x_y: Snake initialization now correct

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -5,7 +5,7 @@ from snake import Snake
 class SnakeTest(unittest.TestCase):
     def test_set_x_y(self):
         """Test that snake x and y coordinates correctly updated."""
-        snake = Snake(0, 0)
+        snake = Snake([0, 0])
         # Check that x and y coordinate changes are correct
         x, y = 0, 0
         snake.direction = 'U'
