a4d89568cda31ea5d2224fc7f332b4d08612299b
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-09-01 08:36:16 +0300

    Added nocapture flag to test script

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source .env/bin/activate
-nosetests -v --with-coverage --cover-package=game,apple,snake
+nosetests -v --with-coverage --nocapture --cover-package=game,apple,snake
 deactivate
