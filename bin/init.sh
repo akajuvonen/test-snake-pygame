049a4936a8138ecbd46ba90b6784649d5f3bf2c6
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-10-19 19:08:51 +0900

    Remove redundant bin/

--- bin/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d .env ]; then
-  virtualenv .env --no-site-packages
-  source .env/bin/activate
-  pip install -r requirements.txt
-  deactivate
-fi;
--- bin/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source .env/bin/activate
-nosetests -v --with-coverage --nocapture --cover-package=game,apple,snake
-deactivate
